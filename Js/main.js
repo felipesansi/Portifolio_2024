dda50d7a34e1720e676caafb3114b5cabeddd311
Felipe Freitas dos Santos Silva <82713076+felipesansi@users.noreply.github.com>
2024-06-28 15:35:35 -0300

    menu mobile funcionando

--- Js/main.js
+++ Js/main.js
@@ -6,3 +6,13 @@ function AbrirMenu() {
       menuMobile.classList.add('abrir');
   }
 }
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('abrir')) {
+      menuMobile.classList.remove('abrir');
+      document.querySelector('.icone').src = "/Imagens/Icone-menu.svg";
+  } else {
+      menuMobile.classList.add('abrir');
+      document.querySelector('.icone').src = "/Imagens/Icone-menu-fechar.svg";
+  }
+}
